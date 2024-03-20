4e04b284e8475dbbe0ea7dd3da03e51a66389052
raiot <raiot.lee@hotmail.com>
2024-03-20 15:59:58 +0800

    feat: style

--- CourseField/settings.py
+++ CourseField/settings.py
@@ -120,9 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/'
 MEDIA_ROOT = BASE_DIR
